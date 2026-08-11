ef7e735fcd57c69566f3719ee3a4330c102a3461
HetalSolanki5 <solankihetal5805@gmail.com>
2026-08-11 10:19:51 +0530

    Hello

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var http=require('http');
 http.createServer(function(req,res)
 {
     res.wtiteHeader(200,{'Content-Type':'text/plain'});
-    res.end("Hello World! I Am Hetu!!!!....");
+    res.end("Hello World! I A!!!....");
 
 }).listen(8500);
 console.log("Server is running https://localhost:8500");
