591230d6ece9f5f10884d7bef95c8d2fcded6a4a
HetalSolanki5 <solankihetal5805@gmail.com>
2026-08-11 10:08:13 +0530

    I Don't know anything

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-const express;
+var http=require('http');
+http.createServer(function(req,res)
+{
+    res.wtiteHeader(200,{'Content-Type':'text/plain'});
+    res.end("Hello World! I Am Hetu!!!!....");
+
+}).listen(8500);
+console.log("Server is running https://localhost:8500");
